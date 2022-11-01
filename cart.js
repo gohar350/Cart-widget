d98ed103bfbeb6dd1208898ecf42aa8f9f202f10
Gohar Butt <57588929+gohar350@users.noreply.github.com>
2022-11-01 18:27:51 +0500

    jquerry missing

--- cart.js
+++ cart.js
@@ -31,11 +31,13 @@ for(let i=0; i<carts.length; i++){
 
     carts[i].addEventListener('click',() => {
         console.log('Added');
-        cartnumber();
+        cartnumber(pro[i]);
+        totalcost(pro[i]);
     })
 }
 
-function cartnumber(){
+function cartnumber(pro){
+    console.log("Product =",pro)
     let pnumber =localStorage.getItem('cartnumber');
     pnumber=parseInt(pnumber);
     if (pnumber){
@@ -46,6 +48,7 @@ function cartnumber(){
     localStorage.setItem('cartnumber',1);
     document.querySelector('.cart span').textContent =1;
     }
+    setItem(pro);
 }
 function onloadcartnumbers(){
     let pnumber =localStorage.getItem('cartnumber');
@@ -53,4 +56,61 @@ function onloadcartnumbers(){
         document.querySelector('.cart span').textContent =pnumber;
     }
 }
+
+
+function setItem(pro){
+    console.log("setItem is Running");
+    let cartItem = localStorage.getItem("ProductsInCart"); 
+    cartItem= JSON.parse(cartItem); //For making Json Object
+    if(cartItem != null){
+        if(cartItem[pro.tag] == undefined){
+            cartItem={
+                ...cartItem,
+                    [pro.tag]: pro
+                
+            }
+        }
+        cartItem[pro.tag].inCart += 1;
+    }
+    else{
+    pro.inCart =1;
+    cartItem={
+        [pro.tag]: pro
+    }
+    }
+
+    localStorage.setItem("ProductsInCart", JSON.stringify (cartItem));
+
+}
+
+function totalcost(pro){
+    let cartcost=localStorage.getItem("TotalCost");
+
+    if(cartcost !=null){
+        cartcost=parseInt(cartcost);
+        localStorage.setItem("TotalCost", cartcost +pro.price);
+
+    }
+    else{
+    localStorage.setItem("TotalCost" , pro.price);
+    }
+}
+function Displaycart(){
+ let cartItem=localStorage.getItem("ProductsInCart");
+ cartItem=JSON.parse(cartItem);
+ let procont=document.querySelector(".products-container");
+ if(cartItem && procont){
+    procont.innerHTML="";
+    Object.values(cartItem).map(item =>{
+
+        procont.innerHTML += `
+        <div class="products">
+            <img src="./$(item.tag).jpg" >
+            <span>$(item.name)</span>
+        </div>        
+        `
+    })
+ }
+}
+Displaycart();
 onloadcartnumbers();
